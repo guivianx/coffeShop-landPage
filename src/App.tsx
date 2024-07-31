c5e9d877fa72eddc34c9b0a4f7c7942886848e8f
Guilherme Viana <guivianx@gmail.com>
2024-07-31 12:05:35 -0300

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
-  
+import styled from 'styled-components'
+import './index.css'
 
+function App() {
   return (
-    <></>
+      <Main>
+        <h1>Hello World</h1>
+      </Main>
   )
 }
 
 export default App
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
