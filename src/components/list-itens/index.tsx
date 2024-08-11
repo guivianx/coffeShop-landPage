d2697f22c704093197f79ead0f7c5bdd36cd54be
Guilherme Viana <guivianx@gmail.com>
2024-08-11 16:53:25 -0300

    products in <UL>

--- src/components/list-itens/index.tsx
+++ src/components/list-itens/index.tsx
@@ -1,15 +1,20 @@
 import productsJson from "../../json/data.json"
 import { FunctionComponent } from "react";
 import { CardItem } from "../item";
+import './index.css'
 
-export const ListItens: FunctionComponent = () => {
+interface ButtonProps {
+    addToCart: Function,
+    cartItems: object[],
+
+}
+
+export const ListItens: FunctionComponent<ButtonProps> = ({ addToCart, cartItems }) => {
     return (
         <div>
-            <h1>Desserts</h1>
-            <ul>
-                {productsJson.data.map((item, index) => (
-                    <CardItem key={index} data={item} />
-                ))}
+            <h1>Desserts</h1> 
+            <ul className="list-products">
+                {productsJson.data.map((item, index) => ( <CardItem key={index} data={item} addToCart={addToCart} cartItems={cartItems} /> ))}
             </ul>
         </div>
     )
