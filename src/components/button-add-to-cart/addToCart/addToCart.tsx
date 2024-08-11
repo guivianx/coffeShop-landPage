92d9f2bc6f403ed0872cf340ee7de2f70a0a7c9a
Guilherme Viana <guivianx@gmail.com>
2024-08-11 16:48:18 -0300

    system cart

--- src/components/button-add-to-cart/addToCart/addToCart.tsx
+++ src/components/button-add-to-cart/addToCart/addToCart.tsx
@@ -1,15 +1,17 @@
 import svgCart from "../../../assets/images/icon-add-to-cart.svg"
 import { FunctionComponent } from "react"
+import './index.css'
 
 interface ButtonProps {
-    buttonClick: Function
+    addToCart: Function,
+    data: object
 }
 
-export const ButtonAddToCart: FunctionComponent<ButtonProps> = ({ buttonClick }) => {
+export const ButtonAddToCart: FunctionComponent<ButtonProps> = ({  addToCart, data}) => {
     return (
-        <button className="button-add-to-cart" onClick={() => buttonClick()}>
+        <button className="button-add-to-cart" onClick={() => { addToCart(data) }}>
             <img src={svgCart} alt="delivy" />
-            Add to cart
+            <p className="txt-add-to-cart">Add to Cart</p>
         </button>
     )
 }
