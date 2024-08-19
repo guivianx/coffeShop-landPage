af77af5e69ac877fb536a9ee12c6ec354448b460
Guilherme Viana <guivianx@gmail.com>
2024-08-19 07:03:33 -0300

    create function for cart: change quantity, confirmOrder

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,35 @@ import { ListItens } from './components/list-itens'
 import { Cart } from './components/cart'
 import styled from 'styled-components'
 import { useState } from 'react'
+import { CardConfirm } from './components/card-confirm'
 import './index.css'
 
 function App() {
 
   const [cartItems, setCartItems] = useState<Array<object>>([])
 
-  function addToCart(item: object) {
-    setCartItems([...cartItems, item])
+  const [confirmOrder, setConfirmOrder] = useState(false)
+
+  function addToCart(item: any, confirm: boolean) {
+    confirm ?
+      cartItems.some((product: any) => { return product.name === item.name ? product.quantity = product.quantity + 1 : null }) ? setCartItems([...cartItems]) : setCartItems([...cartItems, item])
+      :
+      cartItems.some((product: any) => { return product.name === item.name && product.quantity > 1 ? product.quantity = product.quantity - 1 : null }) ? setCartItems([...cartItems]) : ''
   }
-  
+
   function removeItem(id: number) {
     const itemRemove = cartItems.splice(id, 1)
     setCartItems(cartItems.filter(item => item !== itemRemove))
   }
 
+
+
   return (
     <Main>
       <Div>
         <ListItens addToCart={addToCart} cartItems={cartItems} />
-        <Cart cartItems={cartItems} removeItem={removeItem}/>
+        <Cart cartItems={cartItems} removeItem={removeItem} setConfirmOrder={setConfirmOrder}/>
+        { confirmOrder ? <CardConfirm cartItems={cartItems} /> : ''}
       </Div>
     </Main>
   )
