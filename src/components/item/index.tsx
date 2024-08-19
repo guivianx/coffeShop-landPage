1b3ebfee14f1fde565c022cf674743fb73dae46e
Guilherme Viana <guivianx@gmail.com>
2024-08-19 07:13:21 -0300

    I will refactor this later

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { ButtonAddToCart } from "../button-add-to-cart/addToCart/addToCart";
 import { ButtonQuantity } from "../button-add-to-cart/cartQuant/cartquant";
 import './index.css'
@@ -13,28 +13,35 @@ interface CardItemProps {
         },
         name: string,
         category: string,
-        price: number
+        price: number,
+        quantity: number
     },
     addToCart: Function,
     cartItems: object[],
 }
 
-
 export const CardItem: FunctionComponent<CardItemProps> = ({ data, addToCart, cartItems }) => {
 
     const selected = cartItems.some((item: any) => item.name === data.name)
 
+        const [quantity, setQuantity] = useState(data.quantity)
+
     return (
+
         <li className="item">
             <img className={selected ? 'selected' : 'Unselected'} src={data.image.desktop} alt={data.name} />
 
-            {selected ? <ButtonQuantity /> : <ButtonAddToCart addToCart={addToCart} data={data} />}
+            { selected ?
+                <ButtonQuantity data={data} setQuantity={setQuantity} itemQuantity={quantity} addToCart={addToCart} />
+                :
+                <ButtonAddToCart addToCart={addToCart} data={data} />
+            }
 
             <p className="category">{data.category}</p>
 
             <p className="name">{data.name}</p>
 
-            <p className="price">${data.price}</p>
+            <p className="price">${data.price.toFixed(2)}</p>
         </li>
     )
 }
