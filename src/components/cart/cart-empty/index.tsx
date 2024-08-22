a5d5274f94f66187dcbc6c1899d6ff9500612a26
Guilherme Viana <guivianx@gmail.com>
2024-08-22 19:28:01 -0300

    className

--- src/components/cart/cart-empty/index.tsx
+++ src/components/cart/cart-empty/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 export const CartEmpty = () => {
     return (
         <div className="cart-empty-div">
-            <img src={svgCake} alt="delivy" />
+            <img src={svgCake} alt="delivy"  className="img-empty"/>
             <p className="cart-empty">You added items will appear here</p>
         </div>
     )
