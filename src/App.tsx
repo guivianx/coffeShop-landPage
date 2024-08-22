985fdc8e855cfc867e30b4cf4f21f462c179a39e
Guilherme Viana <guivianx@gmail.com>
2024-08-22 19:12:44 -0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { CardConfirm } from './components/card-confirm'
 import { ListItens } from './components/list-itens'
 import { Cart } from './components/cart'
-import styled from 'styled-components'
 import { useState } from 'react'
-import { CardConfirm } from './components/card-confirm'
 import './index.css'
+import './app.css'
 
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
     confirm ?
       cartItems.some((product: any) => { return product.name === item.name ? product.quantity = product.quantity + 1 : null }) ? setCartItems([...cartItems]) : setCartItems([...cartItems, item])
       :
-      cartItems.some((product: any) => { return product.name === item.name && product.quantity > 1 ? product.quantity = product.quantity - 1 : null }) ? setCartItems([...cartItems]) : ''
+      cartItems.some((product: any) => { return product.name === item.name && product.quantity > 1 ? product.quantity = product.quantity - 1 : null }) ? setCartItems([...cartItems]) : null
   }
 
   function removeItem(id: number) {
@@ -23,29 +23,15 @@ function App() {
     setCartItems(cartItems.filter(item => item !== itemRemove))
   }
 
-
-
   return (
-    <Main>
-      <Div>
+    <main>
+      <div>
         <ListItens addToCart={addToCart} cartItems={cartItems} />
-        <Cart cartItems={cartItems} removeItem={removeItem} setConfirmOrder={setConfirmOrder}/>
-        { confirmOrder ? <CardConfirm cartItems={cartItems} /> : ''}
-      </Div>
-    </Main>
+        <Cart cartItems={cartItems} removeItem={removeItem} setConfirmOrder={setConfirmOrder} />
+        { confirmOrder ? <CardConfirm cartItems={cartItems} /> : null }
+      </div>
+    </main>
   )
 }
 
 export default App
-
-const Main = styled.main`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 50px 0px;
-  `
-
-const Div = styled.div`
-  display: flex;
-  gap: 40px;
-`
