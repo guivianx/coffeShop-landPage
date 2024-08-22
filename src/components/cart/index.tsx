4c7f35bcd7280fda52bd93a939c9c58c8f3bd609
Guilherme Viana <guivianx@gmail.com>
2024-08-22 19:27:50 -0300

    refactoring css

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,9 +1,8 @@
-import "../../index.css"
 import { CartEmpty } from "./cart-empty"
-import styled from "styled-components"
 import { CartItens } from "./cart-itens"
 import { FunctionComponent } from "react"
 import './index.css'
+import "../../index.css"
 
 
 export const Cart: FunctionComponent<any> = ({ cartItems, removeItem, setConfirmOrder }) => {  
@@ -11,23 +10,18 @@ export const Cart: FunctionComponent<any> = ({ cartItems, removeItem, setConfirm
     let quantityCart: number = cartItems.map((item: any) => item.quantity).reduce((a: any, b: any) => a + b, 0)    
         
     return (
-        <Div>
+        <div className="div-cart">
+
             <h2 className="cart-quantity">You Cart ({quantityCart})</h2>
-            {quantityCart === 0 ? <CartEmpty /> : <CartItens item={cartItems} removeItem={removeItem} setConfirmOrder={setConfirmOrder} /> }            
-        </Div>
+
+            { quantityCart === 0 ? 
+                <CartEmpty /> 
+                : 
+                <CartItens item={cartItems} removeItem={removeItem} setConfirmOrder={setConfirmOrder} /> 
+            }       
+
+        </div>
     )
 }
 
-const Div = styled.div`
-    padding: 20px;
-    background-color: var(--Rose-50);
-    border-radius: 10px;
-    width: 400px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100%;
-
-
-`
 
